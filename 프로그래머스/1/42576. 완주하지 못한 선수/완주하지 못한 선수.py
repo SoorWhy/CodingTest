3dfeca26076c232f3bfda8d14fdb10d0088f469c
Seongmin Kang <123715654+SoorWhy@users.noreply.github.com>
2024-11-16 14:25:42 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 48.92 ms, Memory: 33.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,18 @@
+def solution(participant, completion):
+    answer = ''
+    dic = {}
+    for i in participant:
+        if i in dic:
+            dic[i] += 1
+        else:
+            dic[i] = 1
+            
+    for i in completion:
+        if i in dic:
+            dic[i] -= 1
+        
+    for i in dic:
+        if dic[i] == 1:
+            answer = i
+            
+    return answer
