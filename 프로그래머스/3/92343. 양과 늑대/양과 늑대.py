a5dc6092c6320bfa991170e35f16f085e2a83c15
Seongmin Kang <123715654+SoorWhy@users.noreply.github.com>
2024-11-20 15:51:42 +0900

    [level 3] Title: 양과 늑대, Time: 8.53 ms, Memory: 10.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
@@ -0,0 +1,29 @@
+from collections import deque, defaultdict
+
+def solution(info, edges):
+    answer = 0
+    graph = defaultdict(list)
+    for i in edges:
+        graph[i[0]].append(i[1])
+    
+    
+    sheep = 1
+    wolf = 0
+    visitable = set([])
+    q = deque([(0, sheep, wolf, visitable)])
+
+    while q:
+        curr, sheep, wolf, visitable = q.popleft()
+        answer = max(answer, sheep)
+        
+        for i in graph[curr]:
+            if i not in visitable:
+                visitable.add(i)
+                
+        for i in visitable:
+            if info[i] == 0:
+                q.append((i, sheep+1, wolf, visitable - {i}))
+            elif info[i] == 1 and sheep > wolf+1:
+                q.append((i, sheep, wolf+1, visitable - {i}))
+        
+    return answer
