452027c9c815e8350d719887bce695450ee108f7
Seongmin Kang <123715654+SoorWhy@users.noreply.github.com>
2024-11-20 16:19:46 +0900

    [level 3] Title: 양과 늑대, Time: 8.02 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
@@ -6,12 +6,12 @@ def solution(info, edges):
     for i in edges:
         graph[i[0]].append(i[1])
     
-    
     sheep = 1
     wolf = 0
-    visitable = set([])
+    visitable = set()
     q = deque([(0, sheep, wolf, visitable)])
-
+    print(q)
+    
     while q:
         curr, sheep, wolf, visitable = q.popleft()
         answer = max(answer, sheep)
@@ -19,11 +19,11 @@ def solution(info, edges):
         for i in graph[curr]:
             if i not in visitable:
                 visitable.add(i)
-                
+        
         for i in visitable:
             if info[i] == 0:
                 q.append((i, sheep+1, wolf, visitable - {i}))
-            elif info[i] == 1 and sheep > wolf+1:
+            elif info[i] == 1 and sheep>wolf+1:
                 q.append((i, sheep, wolf+1, visitable - {i}))
-        
+                
     return answer
